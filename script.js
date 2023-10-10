dd33c02d8a31f517cb158d19bd813fc405ab06d9
ANAWARdev <dridi93anouar@gmail.com>
2023-10-10 09:54:53 +0200

    commentaires

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ let compteur = 0;
     compteur = compteur + 1;
         document.getElementById("counter").innerHTML = compteur;
     }
+    //doIncrease(); si je mets cette fonction sur le code elle va se déclencher 
+    //toute seule puisqu'on l'a appelée, mais il faut mettre lees documents appelés en haut en commentaire ou on les met pas
+    //il va commencé de +1 pas 0
+
     function doDecrease() {
     compteur = compteur - 1;
     document.getElementById("counter").innerHTML = compteur;
